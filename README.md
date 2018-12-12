224801183c1a2521a88660285c1a110a6342dc25
heyingcai <531276338@qq.com>
2018-12-12 23:27:01 +0800

    添加基础类

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # frpc
-Netty RPC Framework
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/dempeZheng/forest/blob/master/LICENSE)
+
+
+>frpc是一款基于Netty的轻量级高性能分布式RPC服务框架。
+
---
+++ frpc-common/src/main/java/com/jibug/frpc/core/common/model/FrpcResponse.java
@@ -0,0 +1,61 @@
+package com.jibug.frpc.core.common.model;
+
+import java.io.Serializable;
+
+/**
+ * @author heyingcai
+ */
+public class FrpcResponse implements Serializable {
+
+    /**
+     * 响应消息id
+     */
+    private String responseId;
+
+    /**
+     * 请求消息id
+     */
+    private String requestId;
+
+    /**
+     * 响应结果
+     */
+    private Object result;
+
+    /**
+     * 错误结果
+     */
+    private String errMsg = "";
+
+    public String getResponseId() {
+        return responseId;
+    }
+
+    public void setResponseId(String responseId) {
+        this.responseId = responseId;
+    }
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+}
