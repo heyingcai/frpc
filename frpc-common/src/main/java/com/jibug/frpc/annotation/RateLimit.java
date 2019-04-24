c9f26b824ea1829ddf76e5434d8e79efb3ebf91d
heyingcai <admin@jibug.com>
2019-04-24 18:26:52 +0800

    add annotation

---
+++ frpc-common/src/main/java/com/jibug/frpc/annotation/RateLimit.java
@@ -0,0 +1,14 @@
+package com.jibug.frpc.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RateLimit {
+
+    int value();
+
+}
