94ff76041992540c90f5426aaea3b7270a89b42e
heyingcai <531276338@qq.com>
2019-05-22 22:26:30 +0800

    update common package

---
+++ frpc-common/src/main/java/com/jibug/frpc/common/config/MethodConfig.java
@@ -0,0 +1,7 @@
+package com.jibug.frpc.common.config;
+
+/**
+ * @author heyingcai
+ */
+public class MethodConfig {
+}
--- frpc-common/src/main/java/com/jibug/frpc/common/config/ProviderConfig.java
+++ frpc-common/src/main/java/com/jibug/frpc/common/config/ProviderConfig.java
@@ -1,5 +1,9 @@
 package com.jibug.frpc.common.config;
 
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author heyingcai
  */
@@ -7,6 +11,22 @@ public class ProviderConfig<T> {
 
     private transient T reference;
 
+    private Map<Method,MethodConfig> methodConfig = new ConcurrentHashMap<>();
 
 
+    public T getReference() {
+        return reference;
+    }
+
+    public void setReference(T reference) {
+        this.reference = reference;
+    }
+
+    public Map<Method, MethodConfig> getMethodConfig() {
+        return methodConfig;
+    }
+
+    public void setMethodConfig(Map<Method, MethodConfig> methodConfig) {
+        this.methodConfig = methodConfig;
+    }
 }
