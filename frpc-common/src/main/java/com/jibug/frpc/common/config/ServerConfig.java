05479585cbcda09a2083ebf336b08908863ab32e
heyingcai <531276338@qq.com>
2019-06-13 23:08:44 +0800

    update common package

--- frpc-common/src/main/java/com/jibug/frpc/common/config/ServerConfig.java
+++ frpc-common/src/main/java/com/jibug/frpc/common/config/ServerConfig.java
@@ -17,6 +17,9 @@ public class ServerConfig {
 
     private int threadPoolMax = getIntValue(SERVER_THREAD_POOL_MAX);
 
+    public ServerConfig() {
+    }
+
     public ServerConfig(int port, String contextPath, int threadPoolCore, int threadPoolMax) {
         this.port = port;
         this.contextPath = contextPath;
