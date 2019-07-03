ea5e59ebab6eb9f03e832b9dec1743a789e05011
heyingcai <531276338@qq.com>
2019-07-03 23:32:15 +0800

    update common package

--- frpc-common/src/main/java/com/jibug/frpc/common/config/ConsumerConfig.java
+++ frpc-common/src/main/java/com/jibug/frpc/common/config/ConsumerConfig.java
@@ -1,15 +1,19 @@
 package com.jibug.frpc.common.config;
 
+import static com.jibug.frpc.common.config.RpcServerConfig.getStringValue;
+import static com.jibug.frpc.common.constant.ConfigConstants.DEFAULT_TIMEOUT;
+import static com.jibug.frpc.common.constant.ConfigPropertiesKey.SERVER_PROTOCOL;
+
 /**
  * @author heyingcai
  */
-public class ConsumerConfig<T> extends AbstractConfig {
+public class ConsumerConfig extends AbstractConfig {
 
-    private String protocol;
+    private String protocol = getStringValue(SERVER_PROTOCOL);
 
     private String directAddr;
 
-    private int connectTimeout;
+    private int connectTimeout = DEFAULT_TIMEOUT;
 
     public String getProtocol() {
         return protocol;
