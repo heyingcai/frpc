bb7bdbf4aaed1df5c2709a2eeb468a83bd7d1aa9
heyingcai <531276338@qq.com>
2019-08-01 23:15:37 +0800

    完善整个远程组件的调用过程

--- frpc-net/src/main/java/com/jibug/frpc/net/handler/ClientProcessHandler.java
+++ frpc-net/src/main/java/com/jibug/frpc/net/handler/ClientProcessHandler.java
@@ -1,17 +1,22 @@
 package com.jibug.frpc.net.handler;
 
-import com.jibug.frpc.common.model.FrpcRequest;
 import com.jibug.frpc.common.model.FrpcResponse;
+import com.jibug.frpc.common.model.Invoker;
+import com.jibug.frpc.net.Connection;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 /**
  * @author heyingcai
  */
-public class ClientProcessHandler extends SimpleChannelInboundHandler<FrpcRequest<FrpcResponse>> {
+public class ClientProcessHandler extends SimpleChannelInboundHandler<FrpcResponse> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, FrpcRequest<FrpcResponse> msg) throws Exception {
-
+    protected void channelRead0(ChannelHandlerContext ctx, FrpcResponse msg) throws Exception {
+        Connection connection = ctx.channel().attr(Connection.CONNECTION).get();
+        Invoker invoker = connection.removeInvoker(msg.getRequestId());
+        if (invoker != null) {
+            invoker.putResult(msg);
+        }
     }
 }
